1bb336ac7251e91e57370b6abc9ca0c6d86378e9
iuroc <m@iuroc.com>
2025-03-08 16:56:29 +0800

    ♻️ refactor(mixin.ts)：重构addLevels函数以支持hasChild属性并优化递归逻辑

--- src/router/ebook/mixin.ts
+++ src/router/ebook/mixin.ts
@@ -27,6 +27,21 @@ export interface CatalogWithLevel {
     parentId: number | null
     index: number
     level?: number
+    hasChild?: boolean
+}
+
+export function addLevelsOld(catalogs: CatalogWithLevel[], parentId: number | null = null, level: number = 0): CatalogWithLevel[] {
+    return catalogs
+        .filter(catalog => catalog.parentId === parentId)
+        .sort((a, b) => a.index - b.index) // 根据 index 排序
+        .map(catalog => {
+            const updatedCatalog = { ...catalog, level }
+            // 递归处理该项的子项
+            const children = addLevelsOld(catalogs, catalog.id, level + 1)
+            // 返回当前项及其子项
+            return [updatedCatalog, ...children]
+        })
+        .flat()
 }
 
 export function addLevels(catalogs: CatalogWithLevel[], parentId: number | null = null, level: number = 0): CatalogWithLevel[] {
@@ -34,9 +49,12 @@ export function addLevels(catalogs: CatalogWithLevel[], parentId: number | null 
         .filter(catalog => catalog.parentId === parentId)
         .sort((a, b) => a.index - b.index) // 根据 index 排序
         .map(catalog => {
-            const updatedCatalog = { ...catalog, level }
-            // 递归处理该项的子项
             const children = addLevels(catalogs, catalog.id, level + 1)
+            const updatedCatalog = {
+                ...catalog,
+                level,
+                hasChild: children.length > 0 // 判断是否有子项
+            }
             // 返回当前项及其子项
             return [updatedCatalog, ...children]
         })
